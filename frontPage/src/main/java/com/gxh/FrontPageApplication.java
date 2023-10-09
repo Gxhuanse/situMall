44efaa47b28fe076cd5dfc9361476dc8851b36d9
gxhuan <234@qq.com>
2023-10-09 19:18:55 +0800

    前端模块添加

---
+++ frontPage/src/main/java/com/gxh/FrontPageApplication.java
@@ -0,0 +1,13 @@
+package com.gxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontPageApplication.class, args);
+    }
+
+}
