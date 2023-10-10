02ba2ab47bb6b3bebcd12702997dcf0a63cb5d8d
gxhuan <234@qq.com>
2023-10-10 11:24:14 +0800

    前端模板模块添加

---
+++ frontModel/src/main/java/com/gxh/FrontModelApplication.java
@@ -0,0 +1,13 @@
+package com.gxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontModelApplication.class, args);
+	}
+
+}
---
+++ frontModel/src/test/java/com/gxh/FrontModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
